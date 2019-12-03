557cedcbf0f6850d146cd770f57d668c353b553a
Sergey <aminov_yar@mail.ru>
2019-12-03 11:46:49 +0300

    commit v0.01

---
+++ app/src/main/java/com/example/todolist/CreateEventActivity.java
@@ -0,0 +1,15 @@
+package com.example.todolist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_event);
+    }
+
+}
