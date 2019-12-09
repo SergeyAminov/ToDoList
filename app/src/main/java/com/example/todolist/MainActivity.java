76a73c979737fc75e8d0e534fc519aeca7ef6d2c
dina-mika <mikhlina.d@gmail.com>
2019-12-09 09:51:42 +0200

    add rrr

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -13,7 +13,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
+//haha
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
